37e4cdb09e3b72d6e22113b7dafa59ff5052f069
Anthony <s3333113@student.rmit.edu.au>
2016-09-21 19:52:16 +1000

    super basic route and page

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>@yield ('title')</title>
+    <!-- <link rel='stylesheet' href='/resources/'> -->
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
