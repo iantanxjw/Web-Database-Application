23c2e0985f4b9d80f88d0ab4f14b67f860743421
s3502942 <s3502942@student.rmit.edu.au>
2016-09-28 20:18:42 +1000

    added tabs

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -10,26 +10,29 @@
     <div class="container marketing">
 
     <!-- Three columns of text below the carousel -->
-    <div class="row">
-        <div class="col-md-4 text-center">
-            <img class="img-circle" src="http://placehold.it/140x140">
-            <h2>Mobile-first</h2>
-            <p>Tablets, phones, laptops. The new 3 promises to be mobile friendly from the start.</p>
-            <p><a class="btn btn-default" href="#">View details »</a></p>
+        <ul class="tab">
+            <li><a href="#" class="tablinks" onclick="opentabs(event, 'NS')">Now Showing</a></li>
+            <li><a href="#" class="tablinks" onclick="opentabs(event, 'CS')">Coming Soon</a></li>
+        </ul>
+
+        <div id="NS" class="tabcontent">
+            <h3>Now Showing</h3>
+            <div class="row">
+                @for ($i = 0; $i < 12; $i++)
+                    <div class="col-md-3 text-center">
+                        <img class="img-circle" src="http://placehold.it/140x140">
+                    </div>
+                @endfor
+
+            </div><!-- /.row -->
         </div>
-        <div class="col-md-4 text-center">
-            <img class="img-circle" src="http://placehold.it/140x140">
-            <h2>One Fluid Grid</h2>
-            <p>There is now just one percentage-based grid for Bootstrap 3. Customize for fixed widths.</p>
-            <p><a class="btn btn-default" href="#">View details »</a></p>
+
+        <div id="CS" class="tabcontent">
+            <h3>Coming soon</h3>
         </div>
-        <div class="col-md-4 text-center">
-            <img class="img-circle" src="http://placehold.it/140x140">
-            <h2>LESS is More</h2>
-            <p>Improved support for mixins make the new Bootstrap 3 easier to customize.</p>
-            <p><a class="btn btn-default" href="#">View details »</a></p>
-        </div>
-    </div><!-- /.row -->
+
+        <!-- Three columns of text below the carousel -->
+
 
 
     <!-- START THE FEATURETTES -->
@@ -37,23 +40,6 @@
     <hr class="featurette-divider">
 
     <div class="featurette">
-        <img class="featurette-image img-circle pull-right" src="http://placehold.it/512">
-        <h2 class="featurette-heading">Responsive Design. <span class="text-muted">It'll blow your mind.</span></h2>
-        <p class="lead">In simple terms, a responsive web design figures out what resolution of device it's being served on. Flexible grids then size correctly to fit the screen.</p>
-    </div>
-
-    <hr class="featurette-divider">
-
-    <div class="featurette">
-        <img class="featurette-image img-circle pull-left" src="http://placehold.it/512">
-        <h2 class="featurette-heading">Smaller Footprint. <span class="text-muted">Lightweight.</span></h2>
-        <p class="lead">The new Bootstrap 3 promises to be a smaller build. The separate Bootstrap base and responsive.css files have now been merged into one. There is no more fixed grid, only fluid.</p>
-    </div>
-
-    <hr class="featurette-divider">
-
-    <div class="featurette">
-        <img class="featurette-image img-circle pull-right" src="http://placehold.it/512">
         <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Flatness.</span></h2>
         <p class="lead">A big design trend for 2013 is "flat" design. Gone are the days of excessive gradients and shadows. Designers are producing cleaner flat designs, and Bootstrap 3 takes advantage of this minimalist trend.</p>
     </div>
--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -2,14 +2,14 @@
 <hr>
 <footer>
     <p class="pull-right"><a href="#">Back to top</a></p>
-    <p>This Bootstrap layout is compliments of Bootply. · <a href="http://www.bootply.com/62603">Edit on Bootply.com</a></p>
+    <p>This Bootstrap layout is compliments of Bootply. ·<a href="http://www.bootply.com/62603">Edit on Bootply.com</a></p>
 </footer>
 
 </div><!-- /.container -->
 
 <script type='text/javascript' src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 
-
 <script type='text/javascript' src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 
 <script type="text/javascript" src="assets/js/bstemplate.js"></script>
+<script type="text/javascript" src="assets/js/tabs.js"></script>
