e62f585f7889ba512136f8b6c363c97467bf5f1c
Anthony <s3333113@student.rmit.edu.au>
2016-09-30 03:14:57 +1000

    form is now an externally included file

--- resources/views/admin/api_refresh.blade.php
+++ resources/views/admin/api_refresh.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.master')
 @section('title', 'DB update')
 @section('content')
-<p>API REFRESH PAGE</p>
+
+{{ Form::open(['url' => 'updatedb', 'method' => 'POST']) }}
+@include('admin.forms.api_refresh_form')
+{{ Form::close() }}
+
 @endsection('content')
