4bc8529798fd3f777a823623db3f405552514dd0
Anthony <s3333113@student.rmit.edu.au>
2016-09-30 01:56:50 +1000

    added admin controller

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class AdminController extends Controller
+{
+    public function index()
+    {
+        return view("admin.panel");
+    }
+
+    public function add_movie()
+    {
+        return view("admin.add_movie");
+    }
+
+    public function remove_movie()
+    {
+        return view("admin.remove_movie");
+    }
+
+    public function add_session()
+    {
+        return view("admin.add_session");
+    }
+
+    public function remove_session()
+    {
+        return view("admin.remove_session");
+    }
+
+    public function api_refresh()
+    {
+        return view("admin.api_refresh");
+    }
+}
