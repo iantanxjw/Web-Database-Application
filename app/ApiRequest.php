e1b1a098681c3db798e471f744f75d2229275fff
Anthony <s3333113@student.rmit.edu.au>
2016-10-01 00:26:37 +1000

    created model for api requests

---
+++ app/ApiRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ApiRequest extends Model
+{
+    public static function getRequest($request_type)
+    {
+        // add ? for get request
+        $request_type = $request_type . "?";
+
+        // get request options - make sure to put key in your .env!!
+        $data = http_build_query([
+            "api_key" => config("tmdb.api.key"),
+            "language" => "en-US"
+        ]);
+
+        // get the result of the request
+        $json = file_get_contents(config("tmdb.api.url") . $request_type . $data);
+
+        return json_decode($json);
+    }
+}
