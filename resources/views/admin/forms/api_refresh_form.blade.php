3e4d4118afd72fb27851dbff6967e48584fdec63
Anthony <s3333113@student.rmit.edu.au>
2016-10-01 01:43:08 +1000

    removed latest from selection

--- resources/views/admin/forms/api_refresh_form.blade.php
+++ resources/views/admin/forms/api_refresh_form.blade.php
@@ -1,7 +1,6 @@
 {{ Form::label('apilabel', 'Populate DB using:') }}
-{{ Form::select('api', ['latest' => 'Latest',
-                        'now_playing' => 'Now playing',
+{{ Form::select('api', ['now_playing' => 'Now playing',
                         'popular' => 'Popular',
                         'top_rated' => 'Top Rated',
-                        'upcoming' => 'Upcoming'], 'latest') }}
+                        'upcoming' => 'Upcoming'], 'now_playing') }}
 {{ Form::submit('Update')}}
