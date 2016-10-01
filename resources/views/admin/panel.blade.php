736d938424c3653660ec8811c08ee58dfb836d13
Anthony <s3333113@student.rmit.edu.au>
2016-10-02 05:24:39 +1100

    temporarily added db update link

--- resources/views/admin/panel.blade.php
+++ resources/views/admin/panel.blade.php
@@ -12,8 +12,8 @@
     {{-- <li><a href="{{ route('add_movie') }}">Add Movie</a></li>
     <li><a href="{{ route('remove_movie') }}">Remove Movie</a></li>
     <li><a href="{{ route('add_session')}}">Add Session</a></li>
-    <li><a href="{{ route('remove_session') }}">Remove Session</a></li>
-    <li><a href="{{ route('api_refresh') }}">Populate DB from API</a></li> --}}
+    <li><a href="{{ route('remove_session') }}">Remove Session</a></li> --}}
+    <li><a href="{{ route('api_refresh') }}">Populate DB from API</a>!!Temporary!! move to movie management</li>
 </ul>
 
 @endsection
