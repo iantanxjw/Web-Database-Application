b4081a1d560c266355ce66258cd5ec0364f6eb30
ulyanov chua <s3501278@student.rmit.edu.au>
2016-10-01 21:18:52 +1000

    Commented Dropdown

--- resources/views/shared/navbar.blade.php
+++ resources/views/shared/navbar.blade.php
@@ -15,7 +15,7 @@
                     <li><a href="{!! route('index') !!}">Home</a></li>
                     <li><a href="{!! route('about') !!}">About</a></li>
                     <li><a href="{!! route('contact') !!}">Contact</a></li>
-                    <!-- <li><a href="{!! route('register') !!}">Register</a></li> -->
+                    <!-- <li><a href="{!! route('register') !!}">Register</a></li>
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
                         <ul class="dropdown-menu">
@@ -27,7 +27,7 @@
                             <li><a href="#">One more separated link</a></li>
                             <li><a href="{!! route('test') !!}">Test</a></li>
                         </ul>
-                    </li>
+                    </li> -->
                 </ul>
                 <ul class="nav navbar-nav navbar-right container-fluid">
                     <!-- Authentication Links -->
