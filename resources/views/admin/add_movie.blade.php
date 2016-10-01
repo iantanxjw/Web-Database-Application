f0947498ea600efa67e00f57fcd7fce82731f47e
Anthony <s3333113@student.rmit.edu.au>
2016-10-02 05:32:43 +1100

    removed old unused files

--- resources/views/admin/add_movie.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.master')
-@section('title', 'Add a movie')
-@section('content')
-<p>Movie addtion page</p>
-@endsection('content')
--- resources/views/admin/add_session.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.master')
-@section('title', 'Add a session')
-@section('content')
-<p>Add a movie session here</p>
-@endsection('content')
