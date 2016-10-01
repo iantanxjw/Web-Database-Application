ae76a396d80819aca317a566af1b11ecc780bac7
Anthony <s3333113@student.rmit.edu.au>
2016-10-02 04:03:09 +1100

    included messages.blade

--- resources/views/admin/panel.blade.php
+++ resources/views/admin/panel.blade.php
@@ -2,6 +2,8 @@
 @section('title', 'Admin')
 @section('content')
 
+@include('shared.messages')
+
 <ul>
     <li><a href="{{ route('add_movie') }}">Add Movie</a></li>
     <li><a href="{{ route('remove_movie') }}">Remove Movie</a></li>
@@ -10,4 +12,4 @@
     <li><a href="{{ route('api_refresh') }}">Populate DB from API</a></li>
 </ul>
 
-@endsection('content')
+@endsection
--- resources/views/admin/test.blade.php
+++ resources/views/admin/test.blade.php
@@ -2,6 +2,22 @@
 @section('title', 'API check')
 @section('content')
 
-<pre>{{ var_dump($movie) }}</pre>
+@if (count($success) > 0)
+    <div class="alert alert-success">
+        <strong>Success</strong>
+        @foreach ($success as $s)
+            <p>{{ $m }}</p>
+        @endforeach
+    </div>
+@endif
 
-@endsection('content')
+@if (count($failure) > 0)
+    <div class="alert alert-danger">
+        <strong>Failure</strong>
+        @foreach ($failure as $f)
+            <p>{{ $m }}</p>
+        @endforeach
+    </div>
+@endif
+
+@endsection
