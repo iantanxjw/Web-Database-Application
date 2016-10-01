e00e0b45bc308f88cfed03b7432c99fecf452025
Anthony <s3333113@student.rmit.edu.au>
2016-10-02 03:59:50 +1100

    added more values

--- config/tmdb.php
+++ config/tmdb.php
@@ -5,7 +5,9 @@
 return [
     "api" => [
         "url" => "https://api.themoviedb.org/3/movie/",
+        "img_url" => "http://image.tmdb.org/t/p/h632/",
         // make sure to dump the api key in your .env file!!! like: TMDB_API_KEY=<key>
-        "key" => env("TMDB_API_KEY")
+        "key" => env("TMDB_API_KEY"),
+        "default_language" => "en-US"
     ]
 ];
