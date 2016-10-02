9c8958eb9bf7712a29e3c0281d1e900055e60d19
Anthony <s3333113@student.rmit.edu.au>
2016-10-02 19:21:34 +1100

    removed size so that it can be specified later

--- config/tmdb.php
+++ config/tmdb.php
@@ -5,7 +5,7 @@
 return [
     "api" => [
         "url" => "https://api.themoviedb.org/3/movie/",
-        "img_url" => "http://image.tmdb.org/t/p/h632/",
+        "img_url" => "http://image.tmdb.org/t/p/",
         // make sure to dump the api key in your .env file!!! like: TMDB_API_KEY=<key>
         "key" => env("TMDB_API_KEY"),
         "default_language" => "en-US"
