065c75eb40fcdfc5353819d95ceb7b8978e8c9a9
Anthony <s3333113@student.rmit.edu.au>
2016-10-04 18:44:54 +1100

    readded temp admin link

--- resources/views/shared/navbar.blade.php
+++ resources/views/shared/navbar.blade.php
@@ -15,7 +15,7 @@
                     <li><a href="{!! route('index') !!}">Home</a></li>
                     <li><a href="{!! route('about') !!}">About</a></li>
                     <li><a href="{!! route('contact') !!}">Contact</a></li>
-
+                    <li><a href="{{ route('admin') }}">Admin</a></li>
                 </ul>
                 <ul class="nav navbar-nav navbar-right container-fluid">
                     <!-- Authentication Links -->
