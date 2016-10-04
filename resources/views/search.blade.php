2bcdf4f6673a98bf243fabfb25539e2036f5bb96
Ulyanov Chua <s3501278@student.rmit.edu.au>
2016-10-04 19:47:53 +1100

    search page

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,6 +1,17 @@
 @extends('layouts.master')
 @section('content')
     <div class="container">
+        <div class="row">
+            <div class="col-md-12 col-md-offset-0">
+                <div class="panel panel-default">
+                    <div class="panel-heading">search</div>
+                    <div class="panel-body">
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
 
     </div>
 
