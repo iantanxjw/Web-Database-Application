e195c9613cc129ee24cdad3022f99de7556fa579
Anthony <s3333113@student.rmit.edu.au>
2016-10-04 19:41:51 +1100

    initial

---
+++ app/DatabaseRequest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+use App\Movies;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DatabaseRequest extends Model
+{
+    public function __construct()
+    {
+
+    }
+
+    public function getData()
+    {
+
+    }
+}
