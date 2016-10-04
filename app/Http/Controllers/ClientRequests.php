f3dff7e1e8b6a86b3c908d17934c74cc363e8fd1
Anthony <s3333113@student.rmit.edu.au>
2016-10-04 21:33:35 +1100

    lazy way

--- app/Http/Controllers/ClientRequests.php
+++ app/Http/Controllers/ClientRequests.php
@@ -17,16 +17,6 @@ class ClientRequests extends Controller
 
         foreach ($movies as $movie)
         {
-            /*$json[] = [
-                "id" => $movie->getID(),
-                "title" => $movie->getTitle(),
-                "overview" => $movie->getDescription(),
-                "release_date" => $movie->getReleaseDate(),
-                "popularity" => $movie->getPopularity(),
-                "genre" => $movie->getGenre(),
-                "poster" => $movie->getPoster(),
-                "background" => $movie->getBackground()
-            ];*/
             $json[] = $movie->getVars();
         }
 
