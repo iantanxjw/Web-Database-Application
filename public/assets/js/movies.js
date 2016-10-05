60582991f57cd2d1d7390afafc98f198128db531
rmit-s3510210-nancy-do <s3510210@student.rmit.edu.au>
2016-10-05 21:31:23 +1100

    updated upcoming movies

--- public/assets/js/movies.js
+++ public/assets/js/movies.js
@@ -8,12 +8,16 @@ $(function() {
             if(i==0) { $("#upcoming").append("<div class='row'>");}
             if (details.poster == null)
             {
-                $("#upcoming").append("<div class='col-sm-3 text-center modal_pop'><p>Poster not available</p><h3>" + details.title +"</h3><br></div>");
+                $("#upcoming").append("<a class='col-sm-3 text-center UC_modalpop' href='#modal' name = '"+details.title+
+                    "'><p>Poster not available</p><h3>" + details.title +"</h3><br></a>");
             }
             else
             {
-                $("#upcoming").append("<a class='col-sm-3 text-center modal_pop' href='#modal'><img class='img-responsive' src='http://image.tmdb.org/t/p/w185/" + details.poster + "'alt='poster'><br><p>"+details.title+"</p></a></div>");
-            }
+                $("#upcoming").append("<a class='col-sm-3 text-center UC_modalPop' href='#modal' name='"+details.title+
+                    "'><div class='polaroid'>" +
+                    "<img class='img-responsive' src='http://image.tmdb.org/t/p/w185/" +
+                    details.poster +
+                    "'alt='poster'><div class='p_container'><p>"+details.title+"</p></div></div></a>");}
 
             i++;
             if(i==4) {
@@ -74,6 +78,29 @@ $(function() {
             })
 
         }, "json")
+
+            $("#populate_modal").append("</div>");
+        });
+    });
+
+    $(document).ready(function() {
+        $(document).on("click", ".UC_modalPop", function() {
+            var title = $(this).attr('name');
+            console.log(title);
+
+            $("#populate_modal").html("<div class='featurette'>");
+            $.get("api_request", {type: "upcoming"}, function(movies) {
+                $.each(movies, function(movie, details) {
+                    if (details.title == title)
+                    {
+                        $("#populate_modal").append("<img class='featurette-image pull-left' src='http://image.tmdb.org/t/p/w185/"+
+                            details.poster+"'>" +
+                            "<h1 class='featurette-heading'>"+details.title+"</h1><p class='lead'>"+details.desc+"</p>" +
+                            "<i class='lead fa fa-calendar'> Release Date: "+details.release_date+"</i>");
+                    }
+                })
+
+            }, "json")
 
             $("#populate_modal").append("</div>");
         });
