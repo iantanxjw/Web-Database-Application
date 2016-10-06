d7ffc6db102bdf0070f891d9cf35a58054f07ff7
Anthony <s3333113@student.rmit.edu.au>
2016-10-07 00:46:39 +1100

    added support for 'updated' and only displays divs if > 0

--- resources/views/shared/messages.blade.php
+++ resources/views/shared/messages.blade.php
@@ -1,4 +1,4 @@
-@if (isset($success))
+@if (isset($success) && count($success) > 0)
     <div class="alert alert-success">
         <strong>Success</strong>
         @foreach ($success as $s)
@@ -7,7 +7,16 @@
     </div>
 @endif
 
-@if (isset($failure))
+@if (isset($update) && count($update) > 0)
+    <div class="alert alert-info">
+        <strong>Updated</strong>
+        @foreach ($update as $u)
+            <p>{{ $u }}</p>
+        @endforeach
+    </div>
+@endif
+
+@if (isset($failure) && count($failure) > 0)
     <div class="alert alert-danger">
         <strong>Failure</strong>
         @foreach ($failure as $f)
