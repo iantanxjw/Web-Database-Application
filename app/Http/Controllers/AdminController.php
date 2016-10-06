6139c8e0c8a452b91f2dcb165855351a4ef0bc08
Anthony <s3333113@student.rmit.edu.au>
2016-10-07 00:47:36 +1100

    now supports updating of rows

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -75,25 +75,53 @@ class AdminController extends Controller
         $movies = $apiRequest->request($request->input("api"));
 
         $success = [];
+        $update = [];
         $failure = [];
 
         // now loop over the movies and fill the db
         foreach ($movies as $movie)
         {
             // prevent movies from being added that already exist
-            if (Movies::where("id", $movie->getId())->exists())
+            if (Movies::find($movie->getID())->exists())
             {
-                $failure[] = $movie->getTitle();
+                // get the movie in the database
+                $result = Movies::find($movie->getID());
+
+                // create a movie object to make it easier to compare with
+                // the movie trying to insert
+                $dbMovie = new Movie(
+                    $result->id,
+                    $result->title,
+                    $result->desc,
+                    $result->release_date,
+                    $result->voteAvg,
+                    $result->status,
+                    $result->genre,
+                    $result->poster,
+                    $result->bg
+                );
+
+                // compare with movie you're trying to add
+                if ($movie->getVars() !== $dbMovie->getVars())
+                {
+                    // they differ so update with new cols
+                    $result->update($movie->getVars());
+                    $update[] = $movie->getTitle();
+                }
+                else
+                {
+                    // values don't differ so don't update
+                    $failure[] = $movie->getTitle();
+                }
             }
             else
             {
                 // FML THIS IS ASSOCIATIVE NOT ORDERED
                 Movies::create($movie->getVars());
-
                 $success[] = $movie->getTitle();
             }
         }
 
-        return view("admin.panel", compact("success", "failure"));
+        return view("admin.panel", compact("success", "failure", "update"));
     }   
 }
