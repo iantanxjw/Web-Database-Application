7e6c222c2a614a22ba8bd06e2f396bc8a250cd17
Anthony <s3333113@student.rmit.edu.au>
2016-10-08 21:22:22 +1100

    templated navbar

--- resources/views/shared/navbar.blade.php
+++ resources/views/shared/navbar.blade.php
@@ -1,45 +1,44 @@
-<div class="navbar-wrapper">
-    <div class="container ">
-        <div class="navbar navbar-inverse navbar-static-top ">
 
-            <div class="navbar-header">
-                <a class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </a>
-                <a class="navbar-brand" href="#">Mavericks Inc</a>
-            </div>
-            <div class="navbar-collapse collapse">
-                <ul class="nav navbar-nav">
-                    <li><a href="{!! route('index') !!}">Home</a></li>
-                    <li><a href="{!! route('search') !!}">Search</a></li>
-                    <li><a href="{{ route('admin') }}">Admin</a></li>
-                </ul>
-                <ul class="nav navbar-nav navbar-right container-fluid">
-                    <!-- Authentication Links -->
-                    @if (Auth::guest())
-                        <li><a href="{{ url('/login') }}">Login</a></li>
-                        <li><a href="{{ url('/register') }}">Register</a></li>
-                    @else
-                        <li ><a href="{!! route('icons') !!}"><span class="icon-star-full" ></span></a></li>
-                        <li ><a href="{!! route('form') !!}"><span class="icon-cart" ></span></a></li>
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                My Account <span class="caret"></span>
-                            </a>
+{{-- NEW TEMPLATE BASED NAV --}}
+@extends('layouts.navbar_template')
+@section('header')
+    <a class="navbar-brand" href="{{ route('index') }}">Mavericks Inc</a>
+@endsection
 
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a>{{ Auth::user()->name }}</a></li>
-                                <li><a>My Tickets </a></li>
-                                <li><a>My Wishlist</a></li>
-                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
-                            </ul>
-                        </li>
-                    @endif
-                </ul>
-            </div>
+@section('left_links')
+    <li><a href="{!! route('index') !!}">Home</a></li>
+    <li><a href="{!! route('search') !!}">Search</a></li>
+    <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Admin <span class="caret"></span>
+        </a>
+        <ul class="dropdown-menu" role="menu">
+            <li><a href="{{ route('admin_movies') }}">Movie management</a></li>
+            <li><a href="{{ route('admin_sessions') }}">Session management</a></li>
+            <li><a href="{{ route('admin_locations') }}">Location managment</a></li>
+            <li><a href="{{ route('admin_users') }}">User management</a></li>
+        </ul>
+    </li>
+@endsection
 
-        </div>
-    </div><!-- /container -->
-</div><!-- /navbar wrapper -->
+@section('right_links')
+<!-- Authentication Links -->
+    @if (Auth::guest())
+        <li><a href="{{ url('/login') }}">Login</a></li>
+        <li><a href="{{ url('/register') }}">Register</a></li>
+    @else
+        <li ><a href="{!! route('icons') !!}"><span class="icon-star-full" ></span></a></li>
+        <li ><a href="{!! route('form') !!}"><span class="icon-cart" ></span></a></li>
+        <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                My Account <span class="caret"></span>
+            </a>
+
+            <ul class="dropdown-menu" role="menu">
+                <li><a>{{ Auth::user()->name }}</a></li>
+                <li><a>My Tickets </a></li>
+                <li><a>My Wishlist</a></li>
+                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+            </ul>
+        </li>
+    @endif
+@endsection
