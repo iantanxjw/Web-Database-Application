1e7c6be26842cc91cf654092ee417561c72ed61f
rmit-s3510210-nancy-do <s3510210@student.rmit.edu.au>
2016-10-09 19:06:16 +1100

    movies layout change

--- resources/views/admin/forms/movie_form.blade.php
+++ resources/views/admin/forms/movie_form.blade.php
@@ -2,58 +2,58 @@
     <div class="col-xs-12 col-sm-12 col-md-12">
         <div class="form-group">
             {{ Form::label('id', 'ID:') }}
-            {{ Form::text('id') }}
+            {{ Form::text('id', null, array('placeholder' => 'Enter movie id','class' => 'form-control')) }}
         </div>
     </div>
     <div class="col-xs-12 col-sm-12 col-md-12">
         <div class="form-group">
             {{ Form::label('title', 'Title:') }}
-            {{ Form::text('title') }}
+            {{ Form::text('title' , null, array('placeholder' => 'Enter movie title','class' => 'form-control')) }}
         </div>
     </div>
     <div class="col-xs-12 col-sm-12 col-md-12">
         <div class="form-group">
             {{ Form::label('desc', 'Overview:') }}
-            {{ Form::text('desc') }}
+            {{ Form::text('desc' , null, array('placeholder' => 'Enter movie description','class' => 'form-control')) }}
         </div>
     </div>
     <div class="col-xs-12 col-sm-12 col-md-12">
         <div class="form-group">
             {{ Form::label('rel', 'Release date:') }}
-            {{ Form::text('release_date') }}
+            {{ Form::text('release_date' , null, array('placeholder' => 'Enter release date of movie','class' => 'form-control')) }}
         </div>
     </div>
     <div class="col-xs-12 col-sm-12 col-md-12">
         <div class="form-group">
             {{ Form::label('vote', 'Vote average:') }}
-            {{ Form::number('voteAvg') }}
+            {{ Form::number('voteAvg', null, array('placeholder' => 'vote average / 10','class' => 'form-control')) }}
         </div>
     </div>
     <div class="col-xs-12 col-sm-12 col-md-12">
         <div class="form-group">
             {{ Form::label('status', 'Status:') }}
-            {{ Form::text('status') }}
+            {{ Form::text('status', null, array('placeholder' => 'showing/not showing/upcoming','class' => 'form-control')) }}
         </div>
     </div>
     <div class="col-xs-12 col-sm-12 col-md-12">
         <div class="form-group">
             {{ Form::label('genre', 'Genre:') }}
-            {{ Form::text('genre') }}
+            {{ Form::text('genre', null, array('class' => 'form-control')) }}
         </div>
     </div>
     <div class="col-xs-12 col-sm-12 col-md-12">
         <div class="form-group">
             {{ Form::label('poster', 'Poster:') }}
-            {{ Form::text('poster') }}
+            {{ Form::text('poster', null, array('placeholder' => 'Enter url','class' => 'form-control')) }}
         </div>
     </div>
     <div class="col-xs-12 col-sm-12 col-md-12">
         <div class="form-group">
             {{ Form::label('bg', 'Background:') }}
-            {{ Form::text('bg') }}
+            {{ Form::text('bg', null, array('placeholder' => 'Enter url','class' => 'form-control')) }}
         </div>
     </div>
     <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-        {{ Form::submit('Update', ['class' => 'btn btn-primary']) }}
+        {{ Form::submit('Add/Update Movie', ['class' => 'btn btn-primary']) }}
     </div>
 </div>
--- resources/views/admin/movies.blade.php
+++ resources/views/admin/movies.blade.php
@@ -3,72 +3,12 @@
 @section('content')
 
     <div class="container">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Movies Management</div>
-                    <div class="panel-body">
-
-
-                        <div class = "create_button">
-                            <a class="btn btn-success create-form" href="#create"> Create New Theatre</a>
-                        </div>
-
-                        @if ($message = Session::get('success'))
-                            <div class="alert alert-success">
-                                <p>{{ $message }}</p>
-                            </div>
-                        @endif
-
-                        <table class="admin_tables" align="center">
-                            <tr>
-                                <th>ID</th>
-                                <th>Title</th>
-                                <th>Overview</th>
-                                <th>Release date</th>
-                                <th>Vote average</th>
-                                <th>Status</th>
-                                <th>Genre</th>
-                                <th>Poster</th>
-                                <th>Background</th>
-                                <th>Action</th>
-                            </tr>
-                            @foreach ($movieObjects as $movie)
-                                <tr>
-                                    <td>{{ $movie->getID() }}</td>
-                                    <td style="width:150px">{{ $movie->getTitle() }}</td>
-                                    <td style="width:250px">{{ $movie->getDescription() }}</td>
-                                    <td>{{ $movie->getReleaseDate() }}</td>
-                                    <td>{{ $movie->getVoteAvg() }}</td>
-                                    <td>{{ $movie->getStatus() }}</td>
-                                    <td>
-                                        @if (is_array($movie->getGenre()) === true)
-                                            @foreach ($movie->getGenre() as $genre)
-                                                <p>{{ $genre }}</p>
-                                            @endforeach
-                                        @else
-                                            {{ $movie->getGenre() }}
-                                        @endif
-                                    </td>
-                                    <td style="width:150px; word-wrap: break-word;word-break: break-all;table-layout: fixed;">
-                                        {{ $movie->getPoster() }}</td>
-                                    <td style="width:150px; word-wrap: break-word;word-break: break-all;table-layout: fixed;">
-                                        {{ $movie->getBackground() }}</td>
-                                    <td><a class="btn btn-primary" href="{{ route('admin_movies.edit',$movie->id) }}">Edit</a>
-                                        {!! Form::open(['method' => 'DELETE','route' => ['admin_movies.destroy', $movie->id],'style'=>'display:inline']) !!}
-                                        {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
-                                        {!! Form::close() !!}</td>
-                                </tr>
-                            @endforeach
-                        </table>
-                    </div>
-                </div>
-    </div>
-
 <div class="panel panel-default">
     <div class="panel-heading">Movies Management</div>
     <div class="panel-body">
         <div class="links">
-            <a href="#add-movie" class="btn btn-primary show-form">Add movie</a>
-            <a href="{{route ('api_refresh') }}" class="btn btn-primary">Populate DB with API call</a>
+            <a href="#add-movie" class="btn btn-success show-form">Add New Movie</a>
+            <a href="{{route ('api_refresh') }}" class="btn btn-success">Populate DB with API call</a>
         </div>
 
         {{-- Add a movie form --}}
@@ -138,8 +78,10 @@
                             {{ $movie->getGenre() }}
                         @endif
                     </td>
-                    <td>{{ $movie->getPoster() }}</td>
-                    <td>{{ $movie->getBackground() }}</td>
+                    <td style="width:150px; word-wrap:break-word;word-break: break-all;table-layout: fixed;">
+                        {{ $movie->getPoster() }}</td>
+                    <td style="width:150px; word-wrap:break-word;word-break: break-all;table-layout: fixed;">
+                        {{ $movie->getBackground() }}</td>
                     {{-- give each edit btn the id so js can request the details --}}
                     <td><a class="btn btn-primary show-edit" data-id="{{ $movie->getID() }}">Edit</a>
                         {!! Form::open(['method' => 'DELETE','route' => ['admin_movies.destroy', $movie->getID()],'style'=>'display:inline']) !!}
@@ -150,5 +92,6 @@
         </table>
     </div>
 </div>
+    </div>
 
 @endsection
