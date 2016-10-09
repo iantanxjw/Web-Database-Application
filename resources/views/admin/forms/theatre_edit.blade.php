36d182cc6f47dade0894e0e128fa2e04d936cbb4
rmit-s3510210-nancy-do <s3510210@student.rmit.edu.au>
2016-10-09 18:23:32 +1100

    changed edit page and implemented back on edit

--- resources/views/admin/forms/theatre_edit.blade.php
+++ resources/views/admin/forms/theatre_edit.blade.php
@@ -1,7 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
 
+    <link rel="icon" href="assets/img/video.png">
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <title>@yield('title')</title>
+    <meta name="generator" content="Bootply"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="description" content="This is an example layout with carousel that uses the Bootstrap 3 RC styles. " />
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../../assets/css/style.css">
+    <link rel="stylesheet" href="../../assets/css/remodal.css">
+    <link rel="stylesheet" href="../../assets/css/remodal-default-theme.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+
+
+    <!--[if lt IE 9]>
+    <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <link rel="apple-touch-icon" href="/bootstrap/img/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/bootstrap/img/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/bootstrap/img/apple-touch-icon-114x114.png">
+
+</head>
+
+<!-- HTML code from Bootply.com editor -->
+
+<body>
+<div class = "row">
+    @include('shared.navbar')
+</div>
 @section('title', 'Edit Locations')
 
-    <div class="container">
+        <div class="container marketing  edit-margin">
                 <div class="panel panel-default">
                     <div class="panel-heading">Theatre Management</div>
                     <div class="panel-body">
@@ -14,7 +46,7 @@
                                         <h2>Edit Theatre details </h2>
                                     </div>
                                     <div class="pull-right">
-                                        <a class="btn btn-primary create-back" href="#back"> Back</a>
+                                        <a class="btn btn-primary create-back" href="{{ route('admin_locations.index') }}"> Back</a>
                                     </div>
                                 </div>
                             </div>
@@ -37,3 +69,11 @@
                     </div>
                 </div>
     </div>
+</body>
+
+<!-- FOOTER -->
+<hr>
+<footer>
+    <p class="pull-right"><a href="#">Back to top</a></p>
+    <p>This Bootstrap layout is compliments of Bootply. ·<a href="http://www.bootply.com/62603">Edit on Bootply.com</a></p>
+</footer>
