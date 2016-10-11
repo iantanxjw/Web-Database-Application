a48d51636b7005778ee9142cc70b505d55de5f29
Anthony <s3333113@student.rmit.edu.au>
2016-10-11 22:52:20 +1100

    search complete

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,96 +1,15 @@
 @extends('layouts.master')
 @section('title', 'Search')
 @section('content')
-    <script type="text/javascript" src="http://code.jquery.com/jquery.min.js"></script>
-    <script type="text/javascript">
-        $(document).ready(function(){
-            $('input[type="radio"]').click(function(){
-                if($(this).attr("value")=="title"){
-                    $(".search").not(".title").hide();
-                    $(".title").show();
-                }
-                if($(this).attr("value")=="location"){
-                    $(".search").not(".location").hide();
-                    $(".location").show();
-                }
-            });
-        });
-    </script>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12 col-md-offset-0">
-                <div class="panel panel-default">
-                    <div class="panel-body">
 
-                        <h2><strong>Advanced Search</strong></h2>
-                        {{--http://bootsnipp.com/snippets/qAqE9--}}
-                        <form action="" data-toggle="validator">
-                            <div class="form-inline required">
-                                <div class="form-group has-feedback">
-                                    <label class="input-group">
-                                        <span class="input-group-addon">
-                                            <input type="radio" name="moiveRadio" value="title" />
-                                        </span>
+{{ Form::open(['id' => 'search-form']) }}
+    @include('forms.search_form')
+{{ Form::close() }}
 
-                                        <div class="form-control form-control-static">Title Text Search</div>
-                                        <span class="glyphicon form-control-feedback "></span>
-                                    </label>
+<div class="container marketing">
+    <div id="search-result" class="row"></div>
+</div>
 
-                                </div>
-                                <div class="form-group has-feedback ">
-                                    <label class="input-group">
-                                        <span class="input-group-addon">
-                                            <input type="radio" name="moiveRadio" value="location" />
-                                        </span>
-                                        <div class="form-control form-control-static">Location Text Search</div>
-                                        <span class="glyphicon form-control-feedback "></span>
-                                    </label>
-                                </div>
-                            </div>
-
-                            <div class="title search">
-                                <div id="custom-search-input">
-                                    <div class="input-group col-md-12">
-                                        <input type="text" class="form-control input-lg" />
-                                        <span class="input-group-btn">
-                                            <button class="btn btn-info btn-lg" type="button">
-                                                <i class="glyphicon glyphicon-search"></i>
-                                            </button>
-                                        </span>
-                                    </div>
-                                </div>
-
-
-                                <div class="panel panel-default">
-                                    <div class="panel-heading">Most Popular Titles With Plot Matching ""</div>
-                                    <div class="panel-body"></div>
-                                </div>
-                            </div>
-
-                            <div class="location search">
-                                <div id="custom-search-input">
-                                    <div class="input-group col-md-12">
-                                        <input type="text" class="form-control input-lg" />
-                                        <span class="input-group-btn">
-                                            <button class="btn btn-info btn-lg" type="button">
-                                                <i class="glyphicon glyphicon-search"></i>
-                                            </button>
-                                        </span>
-                                    </div>
-                                </div>
-
-
-                                <div class="panel panel-default">
-                                    <div class="panel-body"></div>
-                                </div>
-                            </div>
-
-                        </form>
-
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+@include('shared.modal')
 
 @endsection
