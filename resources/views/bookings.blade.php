705edf5b8b01e4c4bf4ca1b7acf284f89b645b06
Ulyanov Chua <s3501278@student.rmit.edu.au>
2016-10-13 01:21:04 +1100

    fixed bookings validation, added min, ticket total cannot be 0

--- resources/views/bookings.blade.php
+++ resources/views/bookings.blade.php
@@ -9,13 +9,14 @@
             <div class="panel-body">
                     <!-- Text field for theatre number -->
                 <div class="row">
-                    <form name = "myForm" method="post" action="{{route('admin_sessions.store')}}" id="commentForm" onsubmit="return validateForm()">
+                    <form name = "myForm" method="post" action="{{route('admin_sessions.store')}}">
                     <!-- Text field for location name  -->
+                        {{ csrf_field() }}
                     <div class="col-xs-12 col-sm-12 col-md-12">
                         <div class="form-group">
                             <!-- duration could get from movie time? --->
                             <strong>Adult</strong>
-                            {!! Form::number('adult', null, array('class' => 'form-control', 'max' => '10')) !!}
+                            {!! Form::number('adult', 0, array( 'id' => 'Adult','class' => 'form-control', 'max' => '10','min'=>'0')) !!}
                         </div>
                     </div>
 
@@ -24,7 +25,7 @@
                         <div class="form-group">
                             <!-- duration could get from movie time? --->
                             <strong>Child</strong>
-                            {!! Form::number('child', null, array('class' => 'form-control', 'max' => '10')) !!}
+                            {!! Form::number('child', 0, array('id' => 'Child','class' => 'form-control', 'max' => '10','min'=>'0')) !!}
                         </div>
                     </div>
 
@@ -33,7 +34,7 @@
                         <div class="form-group">
                             <!-- duration could get from movie time? --->
                             <strong>Concession/Student</strong>
-                            {!! Form::number('concession', null, array('class' => 'form-control', 'max' => '10')) !!}
+                            {!! Form::number('concession', 0, array('id' => 'Student','class' => 'form-control', 'max' => '10','min'=>'0')) !!}
                         </div>
                     </div>
 
@@ -42,13 +43,13 @@
                         <div class="form-group">
                             <!-- duration could get from movie time? --->
                             <strong>Seniors</strong>
-                            {!! Form::number('senior', null, array('placeholder' => 'Enter number of seats available','class' => 'form-control')) !!}
+                            {!! Form::number('senior', 0, array('id' => 'Senior','class' => 'form-control', 'max' => '10','min'=>'0')) !!}
                         </div>
                     </div>
 
                     <!-- Submit button for form -->
                     <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                        <button type="submit" class="btn btn-primary">Submit</button>
+                        <button type="submit" id = "btn2" class="btn btn-primary">Submit</button>
                     </div>
                     </form>
 
