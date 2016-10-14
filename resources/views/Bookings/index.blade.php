97835be3fbf4764355a5961cda2fcd2d833f8bac
rmit-s3510210-nancy-do <s3510210@student.rmit.edu.au>
2016-10-14 20:31:42 +1100

    added panel to my booking

--- resources/views/Bookings/index.blade.php
+++ resources/views/Bookings/index.blade.php
@@ -1,20 +1,32 @@
 @extends('layouts.master')
 @section('title', 'Bookings')
 @section('content')
+    <div class="container">
+        <div class="panel panel-default">
+            <div class="panel-heading">Bookings</div>
+            <div class="panel-body">
 
     <div class="booking_summary">
         <table align="center">
+            <?php
+            $currMovie = "";
+            ?>
             @foreach ($tickets as $ticket)
-                <tr><th> {{$ticket->getStartTime()}} <br> {{$ticket->getWeekday()}} <br> {{$ticket->getTitle()}}</th>
-                <th>{!! Form::open(['method' => 'DELETE','route' => ['bookings.destroy', $ticket->getBookingID()],'style'=>'display:inline']) !!}
-                    {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
-                    {!! Form::close() !!}</th></tr>
-                <tr>
+                @if ($currMovie != $ticket->getBookingID()))
+                <tr><th colspan="2"> {{$ticket->getTitle()}} <br> {{$ticket->getWeekday()}} <br> {{$ticket->getStartTime()}}</th></tr>
+                <?php
+                $currMovie = $ticket->getBookingID();
+                ?>
+                @endif
+                    <tr>
                     <td>{{ $ticket->getType() }}</td>
                     <td>{{ $ticket->getQty() }}</td>
                 </tr>
             @endforeach
         </table>
+    </div>
+            </div>
+        </div>
     </div>
 
 @endsection
