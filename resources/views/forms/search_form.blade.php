cc757e54cf380412d50b2d35190cf339ffb26f71
WaiTung Yuen <s3372851@student.rmit.edu.au>
2016-10-15 15:09:29 +1100

    Change style content page

--- resources/views/forms/search_form.blade.php
+++ resources/views/forms/search_form.blade.php
@@ -6,9 +6,15 @@
                 <div class="col-xs-12 col-sm-12 col-md-12">
                     <div class="form-group">
                         {{ Form::radio('type', 'movie', true) }} {{-- movie is the default selected option --}}
+<<<<<<< HEAD
                         {{ Form::label('movie', 'Search by Movie') }}
                         {{ Form::radio('type', 'location') }}
                         {{ Form::label('location', 'Search by Location') }}
+=======
+                        {{ Form::label('movie', 'Search by Movie:') }}
+                        {{ Form::radio('type', 'location') }}
+                        {{ Form::label('location', 'Search by Location:') }}
+>>>>>>> 0a2b886e9a2bf1c89c7067150d63110af1f09b58
                     </div>
                 </div>
                 <div class="col-xs-12 col-sm-12 col-md-12">
